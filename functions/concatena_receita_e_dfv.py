4deadd56719b4ab66c8ccd207465b384877cea84
SamuelSGSouza <samuels.g.desouza@gmail.com>
2026-03-12 07:19:02 -0300

    Deploy automático: atualização de sistema

--- functions/concatena_receita_e_dfv.py
+++ functions/concatena_receita_e_dfv.py
@@ -145,8 +145,6 @@ def fase_2_concatenador(sistema, nova_execucao:Status_Execucoe_DB):
 
 
 
-
-
     return verificador_fase_2(sistema, nova_execucao)
 
 def verificador_fase_2(sistema, nova_execucao):
