f6c437a5e796479eb9e0d420e2a7b149facdee80
SamuelSGSouza <samuels.g.desouza@gmail.com>
2026-03-15 21:05:03 -0300

    Deploy automático: atualização de sistema

--- functions/concatena_receita_e_dfv.py
+++ functions/concatena_receita_e_dfv.py
@@ -191,7 +191,7 @@ def fase_2_concatenador(sistema, nova_execucao:Status_Execucoe_DB):
 
                 df_receita = df_receita[df_receita["cnpj"].isin(cnpjs_viaveis)]
                 df_receita.to_csv(os.path.join(path_viabilidades, f"Viabilidade_Primaria_{estado}.csv"), sep=";", index=False)
-                salva_dado(f"Quantidade de Empresas com Viabilidade Primaria no Estado {estado}", len(df_receita_viaveis.index), 'janeiro_2026')
+                salva_dado(f"Quantidade de Empresas com Viabilidade Primaria no Estado {estado}", len(df_receita.index), 'janeiro_2026')
                 pd.DataFrame(columns=df_receita.columns).to_csv(os.path.join(path_viabilidades, f"Viabilidade_Secundaria_{estado}.csv"), sep=";", index=False)
 
                 salva_dado(f"Quantidade de Empresas com Viabilidade Secundaria no Estado {estado}", len(df_receita_mailing_secundario.index), 'janeiro_2026')
