58939419daaf16781612a935f81d1bf474331c6f
SamuelSGSouza <samuels.g.desouza@gmail.com>
2026-03-24 01:32:02 -0300

    Deploy automático: atualização de sistema

--- functions/gerador.py
+++ functions/gerador.py
@@ -16,8 +16,8 @@ def inicia_gerador(sistema="oi"):
 
 def inicia_gerador_mailing_2026(sistema="mailing_2026"):
     nova_execucao = Status_Execucoe_DB.objects.create(sistema=sistema)
-    # fase_2_ok = fase_2_concatenador(sistema=sistema, nova_execucao=nova_execucao)
-    # if fase_2_ok:
-    fase_3_ok = fase_3_define_credito(sistema=sistema, nova_execucao=nova_execucao)
-    if fase_3_ok:
-        fase_4_enriquecer(sistema=sistema, nova_execucao=nova_execucao)
+    fase_2_ok = fase_2_concatenador(sistema=sistema, nova_execucao=nova_execucao)
+    if fase_2_ok:
+        fase_3_ok = fase_3_define_credito(sistema=sistema, nova_execucao=nova_execucao)
+        if fase_3_ok:
+            fase_4_enriquecer(sistema=sistema, nova_execucao=nova_execucao)
