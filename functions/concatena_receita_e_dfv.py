0d0acc5efa78011c898563fe06cf108ec04b2df5
SamuelSGSouza <samuels.g.desouza@gmail.com>
2026-05-15 09:38:37 -0300

    Deploy automático: atualização de sistema

--- functions/concatena_receita_e_dfv.py
+++ functions/concatena_receita_e_dfv.py
@@ -295,7 +295,7 @@ def fase_2_concatenador(sistema, nova_execucao:Status_Execucoe_DB):
             
             dfs_dfv = []
             for file in os.listdir(path_arquivos_dfv):
-                if estado in file:
+                if str(estado).lower() in str(file).lower():
 
                     df_dfv_estado = pd.read_excel(os.path.join(path_arquivos_dfv, file), dtype=dtype)
                     dfs_dfv.append(df_dfv_estado)
