317151e58dbd475f558df1e8382ad2fe0a98940d
SamuelSGSouza <samuels.g.desouza@gmail.com>
2026-09-03 18:04:34 -0300

    Deploy automático: atualização de sistema

--- corpdata/urls.py
+++ corpdata/urls.py
@@ -3,6 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('pesquisa_empresa', pesquisa_empresa, name="pesquisa_empresa"),
-
     path('pesquisa_empresas_em_lote', pesquisa_empresas_em_lote, name="pesquisa_empresas_em_lote")
 ]
