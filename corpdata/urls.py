bfa450c8a3cfebcef34d8c74be9220e820e3bf92
SamuelSGSouza <samuels.g.desouza@gmail.com>
2026-09-03 18:07:33 -0300

    Deploy automático: atualização de sistema

--- corpdata/urls.py
+++ corpdata/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
 
+
 urlpatterns = [
     path('pesquisa_empresa', pesquisa_empresa, name="pesquisa_empresa"),
     path('pesquisa_empresas_em_lote', pesquisa_empresas_em_lote, name="pesquisa_empresas_em_lote")
