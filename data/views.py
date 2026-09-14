19a245099e5dc6fc1da9a263c159df4c83d09b66
SamuelSGSouza <samuels.g.desouza@gmail.com>
2026-09-14 16:09:56 -0300

    Deploy automático: atualização de sistema

--- data/views.py
+++ data/views.py
@@ -188,12 +188,12 @@ class Dashboard(LoginRequiredMixin, TemplateView):
     template_name = "dashboard.html"
 
 
-    # def get_context_data(self, **kwargs):
-    #     verifica_atualizacao_receita()
-    #     ctx = super().get_context_data(**kwargs)
-    #     ctx["payload"] = monta_payload_dashboard()
+    def get_context_data(self, **kwargs):
+        verifica_atualizacao_receita()
+        ctx = super().get_context_data(**kwargs)
+        ctx["payload"] = monta_payload_dashboard()
 
-    #     return ctx
+        return ctx
 
 class Status_Execucao(LoginRequiredMixin,TemplateView):
     template_name = "status_execucao.html"
