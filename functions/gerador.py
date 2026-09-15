868f820308e7e4c81611d4df110732515add4a56
SamuelSGSouza <samuels.g.desouza@gmail.com>
2026-09-15 18:00:15 -0300

    Deploy automático: atualização de sistema

--- functions/gerador.py
+++ functions/gerador.py
@@ -92,7 +92,8 @@ def inicia_gerador(sistema="oi"):
     nova_execucao = Status_Execucoe_DB.objects.create(sistema=sistema)
     fase_2_ok = fase_2_concatenador(sistema=sistema, nova_execucao=nova_execucao)
 
-    
+
+
     if fase_2_ok:
         fase_3_ok = fase_3_define_credito(sistema=sistema, nova_execucao=nova_execucao)
         if fase_3_ok:
