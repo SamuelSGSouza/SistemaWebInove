31281dee776f22c973f6bc3afb87ce2d0b98ba89
SamuelSGSouza <samuels.g.desouza@gmail.com>
2026-09-18 15:17:17 -0300

    Deploy automático: atualização de sistema

--- functions/utils.py
+++ functions/utils.py
@@ -1279,6 +1279,7 @@ def complementa_cnpj(pasta_usuario, pasta_destino) -> str:
                     file,
                     dtype=str,
                 )
+                
             df_para_complementar.columns = df_para_complementar.columns.str.lower()
 
             if "cnpj" not in [str(c).lower() for c in df_para_complementar.columns.to_list()]:
